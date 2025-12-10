9271b8fc4ad7dd5c9c032979b466484de5c03ceb
TotallyNotClaritin <cawisnie@terpmail.umd.edu>
2025-12-10 17:11:57 -0500

    Implemented Euler integration

--- physics.h
+++ physics.h
@@ -1,9 +1,13 @@
 //
 // Created by clarion on 12/3/25.
 //
+#pragma once
+
 
 #ifndef UNTITLED_PHYSICS_H
 #define UNTITLED_PHYSICS_H
+#include <utility>
+#include <array>
 
 
 class physics {
@@ -13,6 +17,20 @@ public:
     int timeStep = 1; //1 second
 
 
+    std::pair<std::array<float, 3>,std::array<float, 3>> euler(int mass, int step, std::array<float,3> initPos, std::array<float,3> initVel, std::array<float, 3> force) {
+
+        std::array<float,3> finalPos{0,0,0};
+        std::array<float,3> finalVel{0,0,0};
+
+        for (int i = 0; i < 3; i++) {
+            finalVel[i] = initVel[i] + force[i]/mass * step;
+            finalPos[i] = initPos[i] + initVel[i] * step;
+        }
+
+        auto result = std::make_pair(finalPos,finalVel);
+
+        return result;
+    }
     //IF NECESSARY, adaptive time stepping will be implemented.
 
 };
